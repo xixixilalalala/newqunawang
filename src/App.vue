f242ae4bbf08d518fa7d26e933f8c28ad58bf0c5
xixixilalalala <2647880543@qq.com>
2022-03-17 15:31:30 +0800

    lunbotuwancheng

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <home-header />
+      <home-swiper />
+    </div>
+</template>
+  
+<script setup lang='ts'>
+import HomeHeader from './components/Header.vue'
+import HomeSwiper from './components/Swiper.vue'
+  export default{
+    name:'App',
+    components:{
+      HomeHeader,
+      HomeSwiper
+    }
+  }
+</script>
+  
+<style>
+  
+</style>
